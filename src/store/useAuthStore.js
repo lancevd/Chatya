14013cf2ae072303d32556014a7a999eacf6cd10
lancevd <olamosob@gmail.com>
2025-02-05 20:44:23 +0100

    check Auth state

---
+++ src/store/useAuthStore.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/check");
+
+      set({ authUser: response.data });
+    } catch (error) {
+      console.log("Error checking auth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
