1261fce5e34ce73e940aa7a03c3bd3477bee356d
lancevd <olamosob@gmail.com>
2025-02-05 20:20:41 +0100

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl text-center">Hello World</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
