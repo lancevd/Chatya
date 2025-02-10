2eb39660484627688f50a5dc7241a1001712c75f
lancevd <olamosob@gmail.com>
2025-02-10 17:45:54 +0100

    formed

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { MessageSquare, User, Mail, Eye } from "lucide-react";
+import { MessageSquare, User, Mail, Eye, Lock } from "lucide-react";
 
 const RegisterPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -30,18 +30,22 @@ const RegisterPage = () => {
             </div>
           </div>
         </div>
-        <form
-          onSubmit={handleSubmit}
-          className="space-y-6 w-2/3"
-          action="POST"
-        >
+        <form onSubmit={handleSubmit} className="space-y-6 w-2/3" action="POST">
           <label className="form-control w-full">
             <div className="label">
               <span className="label-text">Full Name</span>
             </div>
             <div className="input input-bordered flex w-full items-center gap-2">
               <User className="size-4" />
-              <input type="text" className="grow" placeholder="John Doe" />
+              <input
+                type="text"
+                className="grow"
+                placeholder="John Doe"
+                value={formData.fullName}
+                onChange={(e) =>
+                  setFormData({ ...formData, fullName: e.target.value })
+                }
+              />
             </div>
           </label>
 
@@ -55,6 +59,10 @@ const RegisterPage = () => {
                 type="text"
                 className="grow"
                 placeholder="example@mail.com"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
               />
             </div>
           </label>
@@ -64,25 +72,18 @@ const RegisterPage = () => {
               <span className="label-text">Password </span>
             </div>
             <div className="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <Lock className="size-4" />
               <input
-                type={`${showPassword} ? 'text' : password`}
+                type={showPassword ? "text" : "password"}
                 className="grow"
-                value="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
               />
               <Eye
-                className="size-4"
+                className="size-4 cursor-pointer"
                 onClick={() => setShowPassword(!showPassword)}
               />
             </div>
