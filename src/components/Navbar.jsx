f60f659acf24510c80f06aab3e9cd7e8103b9d8e
lancevd <olamosob@gmail.com>
2025-02-13 00:43:03 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = () => {
-  const { logout, } = useAuthStore();
+  const { logout, authUser } = useAuthStore();
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -12,36 +12,59 @@ const Navbar = () => {
           <MessageSquare className="size-6 text-primary" /> ChatYa
         </Link>
       </div>
-      <div className="flex-none gap-2">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="User"
-                src="https://res.cloudinary.com/ddydgzuu7/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_white,b_rgb:262c35/v1721993897/samples/smile.jpg"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
+      {authUser ? (
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
             <li>
-              <Link to="/profile">Profile</Link>
+              <a>Link</a>
             </li>
             <li>
-              <Link to="/settings">Settings</Link>
-            </li>
-            <li>
-              <Link onClick={logout}>Logout</Link>
+              <details>
+                <summary>Parent</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
             </li>
           </ul>
         </div>
-      </div>
+      ) : (
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="User"
+                  src="https://res.cloudinary.com/ddydgzuu7/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_white,b_rgb:262c35/v1721993897/samples/smile.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/settings">Settings</Link>
+              </li>
+              <li>
+                <Link onClick={logout}>Logout</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
