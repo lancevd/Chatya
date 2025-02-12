7efe4be65259141d4d600630e9566f9489acfc75
lancevd <olamosob@gmail.com>
2025-02-13 00:38:46 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
+import { MessageSquare } from "lucide-react";
 import React from "react";
+import { Link } from "react-router";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = () => {
+  const { logout, } = useAuthStore();
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to="/" className="btn btn-ghost text-xl flex items-center">
+          <MessageSquare className="size-6 text-primary" /> ChatYa
+        </Link>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown dropdown-end">
@@ -15,8 +21,8 @@ const Navbar = () => {
           >
             <div className="w-10 rounded-full">
               <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                alt="User"
+                src="https://res.cloudinary.com/ddydgzuu7/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_white,b_rgb:262c35/v1721993897/samples/smile.jpg"
               />
             </div>
           </div>
@@ -25,16 +31,13 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
+              <Link to="/profile">Profile</Link>
             </li>
             <li>
-              <a>Settings</a>
+              <Link to="/settings">Settings</Link>
             </li>
             <li>
-              <a>Logout</a>
+              <Link onClick={logout}>Logout</Link>
             </li>
           </ul>
         </div>
