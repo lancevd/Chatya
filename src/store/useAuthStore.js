42c1a7d596766eb2acac5151ed4dc8f06af9b268
lancevd <olamosob@gmail.com>
2025-02-15 10:29:07 +0100

    update function troubleshooting

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
-import { Navigate } from "react-router";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -69,13 +68,14 @@ export const useAuthStore = create((set) => ({
     set({ isUpdatingProfile: true });
 
     try {
-      
+      const response = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: response.data });
       toast.success("Profile updated successfully!");
     } catch (error) {
-      // toast.error(error.response.data.message);
       console.log("Error updating profile:", error);
+      toast.error(error.response.data.message);
     } finally {
       set({ isUpdatingProfile: false });
     }
-  }
+  },
 }));
