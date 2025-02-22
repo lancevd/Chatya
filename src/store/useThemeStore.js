b6227f28111f4dec494ca7daa3f1c68b86d51f5c
lancevd <olamosob@gmail.com>
2025-02-22 16:50:03 +0100

    set theme store

---
+++ src/store/useThemeStore.js
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+const useThemeStore = create((set) => ({
+    theme: localStorage.getItem("chat-theme", theme) || "coffee",
+    setTheme: (theme) => {
+        localStorage.setItem("chat-theme", theme);
+        set({ theme });
+    }
+}))
