18d07f5406070a093fd3bf0236f03ca9aa2881ff
lancevd <olamosob@gmail.com>
2025-02-27 23:06:15 +0100

    message store

---
+++ src/store/useChartStore.js
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChartStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const response = await axiosInstance.get("/messages/users");
+            set({ users: response.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+        set({ isUsersLoading: false });
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const response = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: response.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+        set({ isMessagesLoading: false });
+    },
+}))
