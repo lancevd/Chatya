9e46d37261d0d05dd4043f150a7cf2b29f41c70a
lancevd <olamosob@gmail.com>
2025-02-28 08:18:32 +0100

    Side skeleton

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChartStore } from '../store/useChartStore';
 
 const Sidebar = () => {
+    const {users, getUsers, selectedUser, setSelectedUser, isUserLoading} = useChartStore();
+    const onlineUsers = [];
+
+    useEffect(()=> {
+        getUsers();
+    },[getUsers])
+
+    if (isUserLoading) {
+        return <div>Loading...</div>
+    }
   return (
     <div>Sidebar</div>
   )
