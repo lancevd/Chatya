5fd0c0f289a2269c23d77e5f5d362dac8cb688bf
lancevd <olamosob@gmail.com>
2025-03-01 11:53:43 +0100

    chat header

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChartStore } from "../store/useChartStore";
+import MessageInput from "./MessageInput";
+import ChatHeader from "./ChatHeader";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChartStore();
 
-export default ChatContainer
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [getMessages, selectedUser._id]);
+
+//   if (isMessagesLoading) return <div>Loading...</div>;
+  return (
+    <div className="flex flex-1 flex-col overflow-auto">
+      <ChatHeader />
+      <p>Messages...</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
--- src/store/useChartStore.js
+++ src/store/useChartStore.js
@@ -3,34 +3,34 @@ import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
 export const useChartStore = create((set) => ({
-    messages: [],
-    users: [],
-    selectedUser: null,
-    isUsersLoading: false,
-    isMessagesLoading: false,
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
 
-    getUsers: async () => {
-        set({ isUsersLoading: true });
-        try {
-            const response = await axiosInstance.get("/messages/users");
-            set({ users: response.data });
-        } catch (error) {
-            toast.error(error.response.data.message);
-        }
-        set({ isUsersLoading: false });
-    },
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const response = await axiosInstance.get("/messages/users");
+      set({ users: response.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+    set({ isUsersLoading: false });
+  },
 
-    getMessages: async (userId) => {
-        set({ isMessagesLoading: true });
-        try {
-            const response = await axiosInstance.get(`/messages/${userId}`);
-            set({ messages: response.data });
-        } catch (error) {
-            toast.error(error.response.data.message);
-        }
-        set({ isMessagesLoading: false });
-    },
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const response = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: response.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+    set({ isMessagesLoading: false });
+  },
 
-    // todo: optimize this one later
-    setSelectedUser: selectedUser => set({selectedUser})
-}))
+  // todo: optimize this one later
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
