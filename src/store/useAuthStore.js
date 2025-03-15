0bb1230c1dd9c41a97aa13e0a2a852450024a7b3
lancevd <olamosob@gmail.com>
2025-03-15 20:23:21 +0100

    url

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,6 +3,11 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001"
+    : "https://chatya-backend.vercel.app";
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
@@ -90,7 +95,7 @@ export const useAuthStore = create((set, get) => ({
   connectSocket: () => {
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
-    const socket = io("http://localhost:5001", {
+    const socket = io(BASE_URL, {
       query: { userId: authUser._id },
     });
     socket.connect();
